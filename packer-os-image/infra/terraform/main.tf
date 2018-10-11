b5c36da6a4c1b61614d6608d618faed51ad4f28b
Ed Bulatov <edbulatov@gmail.com>
2018-10-11 22:25:29 +0000

    Add variable declaration for Terraform

--- packer-os-image/infra/terraform/main.tf
+++ packer-os-image/infra/terraform/main.tf
@@ -1,47 +1,55 @@
 provider "google" {
-	project = "infra-219117"
-	region = "europe-west1"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-	name = "reddit-app"
-	machine_type = "f1-micro"
-	zone = "europe-west1-b"
-	tags = ["reddit-app"]
-	boot_disk {
-		initialize_params {
-			image = "reddit-base-1539281516"
-		}
-	}
-	network_interface {
-		network = "default"
-		access_config {}
-	}
-	metadata {
-		sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
-	}
-	connection {
-		type = "ssh"
-		user = "appuser"
-		agent = false
-		private_key = "${file("~/.ssh/appuser")}"
-	}
-	provisioner "file" {
-		source = "files/puma.service"
-		destination = "/tmp/puma.service"
-	}
-	provisioner "remote-exec" {
-		script = "files/deploy.sh"
-	}
+  name         = "reddit-app"
+  machine_type = "f1-micro"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    sshKeys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-	name = "allow-puma-default"
-	network = "default"
-	allow {
-		protocol = "tcp"
-		ports = ["9292"]
-	}
-	source_ranges = ["0.0.0.0/0"]
-	target_tags = ["reddit-app"]
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
 }
